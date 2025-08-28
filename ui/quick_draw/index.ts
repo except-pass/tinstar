7736866ad6535691491c688d6d354d8244a8311d
Will Gathright <gathright@gmail.com>
2025-08-28 12:58:39 +0000

    quick draw details d

--- ui/quick_draw/index.ts
+++ ui/quick_draw/index.ts
@@ -1,5 +1,6 @@
 export { QuickDraw } from './QuickDraw';
 export { quickDrawRegistry, QuickDrawRegistry } from './QuickDrawRegistry';
 export { useQuickDrawActions } from './useQuickDrawActions';
+export { useQuickDrawDetailsActions } from './useQuickDrawDetailsActions';
 export { HelpContextOverlay } from './HelpContextOverlay';
 export type { KeyBinding, ActionRegistry, NamespaceInfo } from './QuickDrawRegistry';
